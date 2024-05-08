262ca88c32fa0dd12ff9ee8f80a50251a53851dd
khaledtarek12 <tareqkhalid092@gmail.com>
2024-05-08 22:07:29 +0300

    enter sound

--- 4th_Task/Assets/Scripts/Movment.cs
+++ 4th_Task/Assets/Scripts/Movment.cs
@@ -16,19 +16,19 @@ public class Movment : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(Vector3.forward * speed * Time.maximumParticleDeltaTime);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * speed * Time.maximumParticleDeltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * speed * Time.maximumParticleDeltaTime);
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(Vector3.back * speed * Time.maximumParticleDeltaTime);
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
     }
 }
